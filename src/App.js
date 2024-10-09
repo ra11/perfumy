41363f3a20dfe6de6bb578f0456e5f21075f19c1
ra11 <rajeswarivinayagam05@gmail.com>
2024-10-09 16:12:31 +0400

    updated perfumy

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar.js"
+import Search from "./components/Search.js"
+import Product from "./components/Product.js"
+import About from "./components/About.js"
+import Footer from "./components/Footer.js"
+
+function App(){
+    return(
+        <div>
+ <Navbar></Navbar>
+    <Search></Search>
+    <Product></Product>
+    <About></About>
+    <Footer></Footer>
+        </div>
+    )
+}
+
+
+
+  export default App
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+function Search() {
+    return (
+      <div className="searchbar">
+        <input type="text" placeholder="Searchbar"></input>
+      </div>
+    )
+  }
+
+  export default Search
